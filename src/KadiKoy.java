e7a3c585098e48893d7166cb0489013228fea29c
MeerimMoidin <ramissara18@gmail.com>
2020-08-06 00:18:20 -0500

    add Kadikoy.java

--- src/KadiKoy.java
+++ src/KadiKoy.java
@@ -2,5 +2,6 @@ public class KadiKoy {
     public static void main(String[] args) {
         System.out.println("Hello Turkey");
         //Welcome to USA from Turkey
+        System.out.println("thank you");
     }
 }
